36e81f12ffd6e5ed64c096ee0a5627b0afbde3f7
Huzaif2004 <huzaifshahul112@gmail.com>
2026-09-22 18:31:34 +0530

    Global Exception Handler added and Exception for CartRestaurantConflict added

--- Backend/src/main/java/com/example/demo/exception/InvalidTotalAmountException.java
+++ Backend/src/main/java/com/example/demo/exception/InvalidTotalAmountException.java
@@ -1,5 +1,10 @@
 package com.example.demo.exception;
 
-public class InvalidTotalAmountException {
+public class InvalidTotalAmountException extends RuntimeException{
+
+	public InvalidTotalAmountException(String message) {
+		super(message);
+	}
+	
 
 }
